40c238fff591665d51662b1592f3926072e68568
rajeev-sr <rajeevkumar16034@gmail.com>
2025-07-30 23:02:42 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
 
-load_dotenv()
+collection = collection()
+app = FastAPI()
 
+# Helper function to convert ObjectId to string
+def serialize_doc(doc):
+    doc["_id"] = str(doc["_id"])
+    return doc
+
+@app.get("/")
+def read_items():
+    items=collection.find()
+    results = [serialize_doc(doc) for doc in items]
+    return results
+
+@app.get("/items/{item_name}")
+def read_item(item_name: str):
+    query = {"name": item_name}
+    items=collection.find(query)
+
+    results = [serialize_doc(doc) for doc in items]
+    return results
+
+@app.post("/items/")
+def create_item(item: dict):
+    id=collection.insert_one(item).inserted_id
+    return {"message": f"Item {id} created successfully"}
+
+@app.put("/items/{item_name}")
+def update_item(item_name: str, item: dict):
+    query = {"name": item_name}
+    collection.update_one(query, {"$set": item})
+    return {"message": f"Item {item_name} updated successfully"}
