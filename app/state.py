faced1d9f75492eb7f6db21f7632b6ff5de36fee
Rahul Raj <rahul.raj9237@gmail.com>
2025-08-05 15:01:06 +0530

    Changes in state

--- app/state.py
+++ app/state.py
@@ -1,12 +1,12 @@
-from typing import TypedDict, Optional, List
+from typing import TypedDict, Optional, List, Dict
 
 class AppState(TypedDict):
     original_query: str
-    analyzed_query: Optional[dict]
+    analyzed_query: Optional[Dict]
     retrieved_docs: Optional[List[str]]
     reranked_docs: Optional[List[str]]
-    web_results: Optional[str]
-    generated_response: Optional[str]
-    final_response: Optional[dict]
-    needs_web_search: Optional[bool]
+    generated_decision: Optional[Dict]
+    final_response: Optional[Dict]
+    critique: Optional[Dict]
     needs_correction: Optional[bool]
+    correction_feedback: Optional[Dict]
