76514dd5060318b5eba94fba288744eb7e813d40
Rahul Raj <rahul.raj9237@gmail.com>
2025-08-09 03:24:22 +0530

    added preporcessing and load_to_db function

--- app/services/graph_service.py
+++ app/services/graph_service.py
@@ -1,12 +1,14 @@
-# /app/services/graph_service.py
 from langgraph.graph import StateGraph, END
 from app.services.nodes import (
+    preprocessing_node,
+    db_loading_node,
     query_analysis_node,
     retrieval_node,
     rerank_node,
     generation_node,
     correction_node,
 )
+from typing import List
 from app.state import AppState
 
 def should_correct(state: AppState) -> str:
@@ -17,11 +19,10 @@ def should_correct(state: AppState) -> str:
     else :
         print("---DECISION: NO CORRECTION NEEDED---")
         return END
-async def execute_graph(query: str) -> dict:
-    """
-    Executes the LangGraph to process the query.
-    """
+async def execute_graph(jobId: str, url: str, questions: List[str]) -> List[dict]:
     workflow = StateGraph(AppState)
+    workflow.add_node("preprocessing", preprocessing_node)
+    workflow.add_node("load_to_db", db_loading_node)
 
     workflow.add_node("query_analysis", query_analysis_node)
     workflow.add_node("retrieve", retrieval_node)
@@ -29,26 +30,25 @@ async def execute_graph(query: str) -> dict:
     workflow.add_node("generate", generation_node)
     workflow.add_node("correct", correction_node)
 
-    workflow.set_entry_point("query_analysis")
+    workflow.set_entry_point("preprocessing")
+    workflow.add_edge("preprocessing", "query_analysis")
+    workflow.add_edge("query_analysis", "load_to_db")
     
-    workflow.add_edge("query_analysis", "retrieve")
+    workflow.add_edge("load_to_db", "retrieve")
     workflow.add_edge("retrieve", "rerank")
     workflow.add_edge("rerank", "generate")
-    workflow.add_conditional_edges(
-        "generate",
-        should_correct,
-        {
-            "correct": "correct",
-            END: END,
-        },
-    )
-    workflow.add_edge("correct", "generate")
+    
+    workflow.add_edge("generate", END)
 
     app_graph = workflow.compile()
 
-    initial_state = {"original_query": query}
+    initial_state = {
+        "jobId": jobId,
+        "document_url": url,
+        "original_questions": questions,
+    }
     final_state = await app_graph.ainvoke(initial_state)
-    if not final_state.get("final_response"):
+    if not final_state.get("final_answers"):
         raise Exception("Graph execution failed to produce a final response.")
 
-    return final_state["final_response"]
+    return final_state["final_answers"]
